049f23e986fb0be906d3f639161f325cc32d6d9d
Lindsea Martin <112672579+LindseaMartin@users.noreply.github.com>
2023-05-03 10:01:57 -0600

    added grid

--- UI/transitioner.gd
+++ UI/transitioner.gd
@@ -8,7 +8,6 @@ class_name Transitioner
 @onready var animation_tex : TextureRect = $TextureRect
 @onready var animation_player : AnimationPlayer = $AnimationPlayer
 
-
 func _ready():
 	animation_tex.visible = false
 
@@ -17,8 +16,6 @@ func set_next_animation(fading_out : bool):
 		animation_player.queue("fade_out")
 	else:
 		animation_player.queue("fade_in")
-
-
 
 func _on_animation_player_animation_finished(anim_name):
 	if (scene_to_load != null && anim_name == scene_switch_anim):
